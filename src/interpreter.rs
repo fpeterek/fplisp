cc8213c84d0071887b819930d096dc62d2ea3529
Filip Peterek <fpeterek@seznam.cz>
2024-05-31 19:40:26 +0200

    Little bit of progress

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,4 +1,6 @@
-use crate::value::Value;
+use core::panic;
+
+use crate::{atom::Atom, value::Value};
 
 
 pub struct Interpreter {
@@ -11,16 +13,37 @@ impl Interpreter {
         Interpreter { }
     }
 
-    fn interpret_statement(&self, stmt: Value) {
+    fn eval_fn(&self, fn_call: &Value) -> Value {
+        Value::Atom { atom: Atom::Nil }
+    }
+
+    fn eval_symbol(&self, symbol: &Atom) -> Value {
+        Value::Atom { atom: Atom::Nil }
+    }
+
+    fn eval_atom(&self, atom: Value) -> Value {
+        match atom {
+            Value::ConsCell { left: _, right: _ } => {
+                panic!("Invalid interpreter state")
+            },
+            Value::Atom { atom: ref value } => match value {
+                Atom::Symbol { value: _ } => self.eval_symbol(value),
+                _ => atom.clone()
+            }
+        }
+    }
+
+    fn interpret_statement(&self, stmt: Value) -> Value {
         match stmt {
-            _ => {},
+            Value::ConsCell { left: _, right: _ } => self.eval_fn(&stmt),
+            Value::Atom { atom: _ } => self.eval_atom(stmt)
         }
 
     }
 
     pub fn interpret(&self, statements: Vec<Value>) {
         for stmt in statements {
-            self.interpret_statement(stmt)
+            self.interpret_statement(stmt);
         }
     }
 }
